38b0bd2acb0202a1c9825a4e22dd544143e5f0ae
Daniel2D <danielpoole1998@gmail.com>
2020-08-12 15:54:17 +0100

    Alternate JS
    Here's an alternate way of doing the JS.
    
    Note on index.html I've used the 'onclick' attribute which calls a function, rather than having to add event listeners onto each of the buttons (this is more efficient in a large app).

--- daniel/daniel.js
+++ daniel/daniel.js
@@ -1,32 +1,26 @@
 // set initial count 
 let count = 0;
 
-//select value and buttons
-const value = document.querySelector('#value');
-const btns = document.querySelectorAll('.btn');
+//select value
+const value = document.getElementById("value");
+// ^^ Using getElementById Instead of querySelector for an element with an ID is more efficient (although doesn't make a difference for small apps).
 
+// Have a function that updates the value and then the colour.
+function updateValue(increment) {
+    count += increment; // Increment/decrement the value (tip, incrementing 2 by -1 = 1, so you can use an increment value instead).
 
-btns.forEach(function(btn){
-    btn.addEventListener("click", function(e){
-        const styles = e.currentTarget.classList;
-        if(styles.contains("decrease")){
-            count --;
-        }
-        else if (styles.contains("increase")){
-            count ++;
-        }
-        else {
-            count = 0;
-        }
-        if(count > 0){
-            value.style.color = "green";
-        }
-        if(count < 0){
-            value.style.color = "red";
-        }
-        if(count === 0){
-            value.style.color = "white"; 
-        }
-        value.textContent = count;
-    })
-});
+    if(count > 0) {
+        value.style.color = "green";
+    } else if(count < 0) {
+        value.style.color = "red";
+    } else {
+        value.style.color = "white"
+    }
+
+    value.textContent = count;
+}
+
+function reset() {
+    count = 0;
+    updateValue(0);
+}
